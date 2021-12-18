0efd4435e1e67d3268ed318bd2ddcd1877a55183
GAAdel <32236445+GAAdel@users.noreply.github.com>
2021-12-18 22:43:43 +0300

    opening cart

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <app-header
+    ></app-header>
+
+    <main>
+      <div class="container">
+        <div class="row">
+
+          <app-filter></app-filter>
+          <app-good></app-good>
+
+        </div>
+      </div>
+    </main>
+
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppGood from './components/AppGood.vue'
+import AppFilter from './components/AppFilter.vue'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+    }
+  },
+  components: { AppHeader, AppGood, AppFilter }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/AppCart.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="cart" :style="{display : this.isShowCart}">
+      <div class="cart-body">
+        <div class="cart-title">Корзина</div>
+        <div class="cart-total">Общая сумма: <span>0</span> руб</div>
+
+        <div class="cart-wrapper">
+          <div id="cart-empty">
+            Ваша корзина пока пуста
+          </div>
+        </div>
+        <button class="btn btn-primary cart-confirm">Оформить заказ</button>
+        <div class="cart-close" @click="closeCart">
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['isShowCart'],
+  emits: ['close-cart'],
+  data () {
+    return {
+    }
+  },
+  methods: {
+    closeCart () {
+      this.$emit('close-cart', 'none')
+    }
+  }
+}
+</script>
